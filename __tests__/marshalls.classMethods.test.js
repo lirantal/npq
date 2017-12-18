6eb1a079536b7fc5e2bc217ba577d54fa75ba5a5
Liran Tal <liran.tal@gmail.com>
2017-12-18 20:50:09 +0200

    test(refactor): use a specific glob mock for each test case

--- __tests__/marshalls.classMethods.test.js
+++ __tests__/marshalls.classMethods.test.js
@@ -1,16 +1,12 @@
-const marshalls = require('../lib/marshalls')
-
-jest.mock('glob', () => {
-  return jest.fn((pattern, options, callback) => {
-    if (pattern === 'testOk') {
-      return callback(null, ['file1', 'file2'])
-    } else {
-      return callback(new Error('some error'), null)
-    }
-  })
-})
-
 test('collecting marshalls resolves with files array', async () => {
+  const marshalls = require('../lib/marshalls')
+
+  jest.mock('glob', () => {
+    return jest.fn((pattern, options, callback) => {
+      return callback(null, ['file1', 'file2'])
+    })
+  })
+
   marshalls.GLOB_MARSHALLS = 'testOk' // trigger success branch
   const marshallFiles = await marshalls.collectMarshalls()
 
@@ -18,12 +14,20 @@ test('collecting marshalls resolves with files array', async () => {
 })
 
 test('collecting marshalls resolves with error if unable to process files', () => {
-  marshalls.GLOB_MARSHALLS = 'error' // trigger failure branch
+  const marshalls = require('../lib/marshalls')
+
+  jest.mock('glob', () => {
+    return jest.fn((pattern, options, callback) => {
+      return callback(new Error('some error'), null)
+    })
+  })
 
   expect(marshalls.collectMarshalls()).rejects.toEqual(expect.any(Error))
 })
 
 test('build marshalls without any should throw error', () => {
+  const marshalls = require('../lib/marshalls')
+
   expect(marshalls.buildMarshallTasks([])).rejects.toEqual(expect.any(Error))
   expect(marshalls.buildMarshallTasks({})).rejects.toEqual(expect.any(Error))
   expect(marshalls.buildMarshallTasks(5)).rejects.toEqual(expect.any(Error))
