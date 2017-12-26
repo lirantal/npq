e705c54b0bd7523b60280ada74ac105e369aacdc
Liran Tal <liran.tal@gmail.com>
2017-12-26 20:09:03 +0200

    feat(marshalls): add download and readme marshalls

--- __tests__/packageRepoUtils.test.js
+++ __tests__/packageRepoUtils.test.js
@@ -9,6 +9,10 @@ jest.mock('axios', () => {
   }
 })
 
+beforeEach(() => {
+  jest.resetModules()
+})
+
 test('repo utils always has a default package registry url', () => {
   const packageRepoUtils = new PackageRepoUtils()
   expect(packageRepoUtils.registryUrl).toBeTruthy()
@@ -20,6 +24,14 @@ test('repo utils constructor allows setting a package registry url', () => {
     registryUrl: pkgRegistryUrl
   })
   expect(packageRepoUtils.registryUrl).toEqual(pkgRegistryUrl)
+})
+
+test('repo utils constructor allows setting a package registry api url', () => {
+  const pkgRegistryApiUrl = 'https://api.npmjs.org'
+  const packageRepoUtils = new PackageRepoUtils({
+    registryApiUrl: pkgRegistryApiUrl
+  })
+  expect(packageRepoUtils.registryApiUrl).toEqual(pkgRegistryApiUrl)
 })
 
 test('repo utils returns a package json object from registry', async () => {
@@ -34,4 +46,35 @@ test('repo utils retrieves package latest version', async () => {
   const packageName = 'testPackage'
   const result = await packageRepoUtils.getLatestVersion(packageName)
   expect(result).toEqual('3.1.0')
+})
+
+test('repo utils retrieves package README information', async () => {
+  const packageRepoUtils = new PackageRepoUtils()
+  const packageName = 'testPackage'
+  const result = await packageRepoUtils.getReadmeInfo(packageName)
+  expect(result).toContain('dockly')
+})
+
+test('repo utils retrieves package download count', async () => {
+  const PackageRepoUtils = require('../lib/helpers/packageRepoUtils')
+
+  jest.mock('axios', () => {
+    return {
+      get: jest.fn(() => {
+        return Promise.resolve({
+          data: {
+            downloads: 1950,
+            start: '2017-11-26',
+            end: '2017-12-25',
+            package: 'express-version-route'
+          }
+        })
+      })
+    }
+  })
+
+  const packageRepoUtils = new PackageRepoUtils()
+  const packageName = 'testPackage'
+  const result = await packageRepoUtils.getDownloadInfo(packageName)
+  expect(result).toEqual(1950)
 })
