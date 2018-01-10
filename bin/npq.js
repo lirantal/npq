86b9cf1d13d2bdec14afd3d30e0198123f8d616a
Liran Tal <liran.tal@gmail.com>
2018-01-10 18:15:34 +0200

    fix(packageManager): explicitly install with package manager. refactor prompt

--- bin/npq.js
+++ bin/npq.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict'
 
+const inquirer = require('inquirer')
+
 const cli = require('../lib/cli')
 const pkgMgr = require('../lib/packageManager')
 const Marshall = require('../lib/marshall')
@@ -9,8 +11,32 @@ const marshall = new Marshall({
   pkgs: cli.package
 })
 
-marshall.process().then(promptInstall => {
-  if (!cli.dryRun && promptInstall.install === true) {
-    pkgMgr.process(cli.packageManager, cli.package)
-  }
-})
+marshall
+  .process()
+  .then(result => {
+    if (cli.dryRun) {
+      process.exit(0)
+    }
+
+    if (result && result.error) {
+      return inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'install',
+          message: 'Would you like to continue installing package(s)?',
+          default: false
+        }
+      ])
+    }
+
+    return { install: true }
+  })
+  .then(status => {
+    if (status && status.hasOwnProperty('install') && status.install === true) {
+      pkgMgr.process(cli.packageManager, cli.package)
+    }
+  })
+  .catch(error => {
+    console.error(error)
+    process.exit(-1)
+  })
