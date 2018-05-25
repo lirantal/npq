05b34991b082134e0aca46333bce165adad1428b
Benedek Gagyi <gagyibenedek@yahoo.com>
2018-05-25 15:57:40 +0200

    feat(marshall): replace url-checker with axios.get

--- __tests__/marshalls.repo.test.js
+++ __tests__/marshalls.repo.test.js
@@ -1,9 +1,7 @@
 const RepoMarshall = require('../lib/marshalls/repo.marshall')
-const urlExists = require('url-exists')
+const axios = require('axios')
 
-jest.mock('url-exists', () => {
-  return jest.fn()
-})
+jest.mock('axios')
 
 const testMarshall = new RepoMarshall({
   packageRepoUtils: {
@@ -82,42 +80,24 @@ test('throws the right error when there is no repo URL in the pkg data', async (
   }
 })
 
-test('formats the URL the right way before checking if it exists', async () => {
-  const pkgData = JSON.parse(JSON.stringify(fullPkgData))
-  await testMarshall.validate(pkgData)
-  expect(urlExists.mock.calls[0][0]).toBe('url')
-
-  pkgData.packageName.versions['1.0.0'].repository.url = 'git://url'
-  await testMarshall.validate(pkgData)
-  expect(urlExists.mock.calls[1][0]).toBe('https://url')
-})
-
 test('throws the right error when the url does not exist', async () => {
-  await testMarshall.validate(fullPkgData)
-  const callback = urlExists.mock.calls[0][1]
+  axios.get.mockImplementationOnce(() =>
+    Promise.reject(new Error('error'))
+  )
   try {
-    callback(null, false)
-  } catch (e) {
-    expect(e.message).toBe('the repository associated with the package (url) does not exist or is unreachable at the moment.')
-  }
-})
-
-test('throws the right error when the url check fails', async () => {
-  await testMarshall.validate(fullPkgData)
-  const callback = urlExists.mock.calls[0][1]
-  try {
-    callback(new Error('the url check failed'))
+    await testMarshall.validate(fullPkgData)
   } catch (e) {
     expect(e.message).toBe('the repository associated with the package (url) does not exist or is unreachable at the moment.')
   }
 })
 
 test('does not throw any errors if the url exists', async () => {
-  await testMarshall.validate(fullPkgData)
-  const callback = urlExists.mock.calls[0][1]
+  axios.get.mockImplementationOnce(() =>
+    Promise.resolve('success')
+  )
   try {
-    callback(null, true)
+    await testMarshall.validate(fullPkgData)
   } catch (e) {
-    throw new Error('The url check should not throw any errors')
+    throw new Error(e, 'The url check should not throw any errors')
   }
 })
