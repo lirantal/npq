59bce9b7edaa5e1520586b8be2363dfba64d1d16
Liran Tal <liran.tal@gmail.com>
2026-08-25 15:52:38 +0000

    test: share CI environment variable definitions

--- __tests__/cliSupportHandler.test.js
+++ __tests__/cliSupportHandler.test.js
@@ -1,37 +1,25 @@
 'use strict'
 
-const { isInteractiveTerminal } = require('../lib/helpers/cliSupportHandler')
-
-const ciEnvVars = [
-  'CI',
-  'CONTINUOUS_INTEGRATION',
-  'BUILD_NUMBER',
-  'RUN_ID',
-  'GITHUB_ACTIONS',
-  'GITLAB_CI',
-  'TRAVIS',
-  'CIRCLECI',
-  'JENKINS_URL',
-  'TEAMCITY_VERSION',
-  'TF_BUILD'
-]
+const { CI_ENV_VARS, isInteractiveTerminal } = require('../lib/helpers/cliSupportHandler')
 
 describe('isInteractiveTerminal', () => {
   const originalStdinTTY = process.stdin.isTTY
   const originalStdoutTTY = process.stdout.isTTY
-  const originalCiEnv = Object.fromEntries(ciEnvVars.map((envVar) => [envVar, process.env[envVar]]))
+  const originalCiEnv = Object.fromEntries(
+    CI_ENV_VARS.map((envVar) => [envVar, process.env[envVar]])
+  )
 
   afterEach(() => {
     process.stdin.isTTY = originalStdinTTY
     process.stdout.isTTY = originalStdoutTTY
-    for (const envVar of ciEnvVars) {
+    for (const envVar of CI_ENV_VARS) {
       if (originalCiEnv[envVar] === undefined) delete process.env[envVar]
       else process.env[envVar] = originalCiEnv[envVar]
     }
   })
 
   test('requires both stdin and stdout to be TTYs', () => {
-    for (const envVar of ciEnvVars) delete process.env[envVar]
+    for (const envVar of CI_ENV_VARS) delete process.env[envVar]
     process.stdin.isTTY = true
     process.stdout.isTTY = true
     expect(isInteractiveTerminal()).toBe(true)
